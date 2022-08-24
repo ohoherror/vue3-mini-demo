d2f14342346ed2d8be8969f71ac46d3912fd756e
ohoherror <1458251589@qq.com>
2022-08-24 17:55:03 +0800

    trigger

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,41 @@
 class ReactiveEffect {
-    private _fn: any;
-    constructor(fn) {
-        this._fn = fn
+    active = true
+    deps = []
+    public onStop?: () => void;
+    constructor(public fn, public scheduler?) {
+        console.log("创建 ReactiveEffect 对象");
     }
     run() {
         activeEffect = this
-        this._fn()
+        return this.fn()
+    }
+    stop() {
+        if (this.active) {
+            if (this.onStop) {
+                this.onStop()
+            }
+            cleanupEffect(this);
+            this.active = false
+        }
     }
 }
 
+function cleanupEffect(effect) {
+    // 找到所有依赖这个 effect 的响应式对象
+    // 从这些响应式对象里面把 effect 给删除掉
+    effect.deps.forEach((dep) => {
+        dep.delete(effect);
+    });
+
+    effect.deps.length = 0;
+}
+
+export function stop(runner) {
+    runner.effect.stop()
+}
+
 const targetMap = new Map()
+//收集依赖 放置在targetMap里面
 export function track(target, key) {
     let depsMap = targetMap.get(target)
     if (!depsMap) {
@@ -21,18 +47,35 @@ export function track(target, key) {
         dep = new Set()
         depsMap.set(key, dep)
     }
-    dep.add(activeEffect)
+    if (!dep.has(activeEffect)) {
+        dep.add(activeEffect);
+        if (activeEffect) {
+            (activeEffect as any).deps.push(dep);
+        }
+    }
 }
-
+//触发依赖
 export function trigger(target, key) {
     let depsMap = targetMap.get(target)
     let dep = depsMap.get(key)
+    console.log(dep)
     for (const effect of dep) {
-        effect.run()
+        if (effect.scheduler) {
+            effect.scheduler()
+        } else {
+            //执行函数，会再次通过get获取值收集依赖
+            effect.run()
+        }
+
     }
 }
 let activeEffect
-export function effect(fn) {
+export const extend = Object.assign;
+export function effect(fn, options: any = {}) {
     const _effect = new ReactiveEffect(fn)
+    extend(_effect, options)
     _effect.run()
+    const runner: any = _effect.run.bind(_effect)
+    runner.effect = _effect
+    return runner
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from '../reactive'
-import { effect } from '../effect'
+import { effect, stop } from '../effect'
 describe('effect', () => {
     it("happy path", () => {
         const user = reactive({ age: 10 });
@@ -13,4 +13,89 @@ describe('effect', () => {
         user.age++;
         expect(nextAge).toBe(12)
     })
+
+    it('should run the passed function once (wrapped by a effect)', () => {
+        const fnSpy = jest.fn(() => { })
+        effect(fnSpy)
+        expect(fnSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it("should observe multiple properties", () => {
+        let dummy
+        const counter = reactive({ num1: 0, num2: 0 })
+        let runner = effect(() => (
+            dummy = counter.num1 + counter.num1 + counter.num2
+        ))
+
+        expect(dummy).toBe(0)
+        stop(runner)
+        //set值会执行trigger,执行函数fn,并触发get,数据得到响应
+        counter.num1 = counter.num2 = 7
+        expect(dummy).toBe(0)
+    })
+
+    it("should return runner", () => {
+        let foo = 10
+        const runner = effect(() => {
+            foo++
+            return 'foo'
+        })
+        expect(foo).toBe(11)
+        const r = runner()
+        expect(foo).toBe(12)
+        expect(r).toBe('foo')
+
+    })
+
+    it("scheduler", () => {
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({ foo: 1 });
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            { scheduler }
+        );
+        console.log(scheduler)
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        //should be called on first trigger
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        // // should not run yet
+        expect(dummy).toBe(1);
+        // // manually run
+        run();
+        // // should have run
+        expect(dummy).toBe(2);
+    });
+
+    it("stop", () => {
+        let dummy;
+        const obj = reactive({ prop: 1 })
+        const runner = effect(() => {
+            dummy = obj.prop
+        })
+
+        obj.prop = 2
+        expect(dummy).toBe(2)
+        stop(runner)
+        obj.prop = 3
+
+        expect(dummy).toBe(2)
+        runner()
+        expect(dummy).toBe(3)
+    })
+
+    it("event:onStop", () => {
+        const onStop = jest.fn()
+        const runner = effect(() => { }, { onStop })
+        stop(runner)
+        expect(onStop).toHaveBeenCalled()
+    })
 })
+
